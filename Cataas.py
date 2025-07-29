67edca786e16bc34575c0788dc1538be7753764e
bmaayandexru <81138044+bmaayandexru@users.noreply.github.com>
2025-07-29 14:26:11 +0300

    Функция для получения изображения

--- Cataas.py
+++ Cataas.py
@@ -1,11 +1,19 @@
 from tkinter import *
-from PIL import Image
+from PIL import ImageTk, Image
 import requests
 from io import BytesIO
 
 
-def load_image():
-
+def load_image(url):
+    try:
+        response = requests.get()
+        response.raise_for_status()
+        image_data = BytesIO(response.content)
+        img = Image.open(image_data)
+        return ImageTk.PhotoImage(img)
+    except Exception as e:
+        print(f"Ошибка {e}")
+        return None
 
 
 root = Tk()
