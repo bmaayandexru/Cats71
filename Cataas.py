3e9199f963a48c95ad96466c8b75c833a75a688a
bmaayandexru <81138044+bmaayandexru@users.noreply.github.com>
2025-07-29 14:47:12 +0300

    Добавлено изменение размера изображения

--- Cataas.py
+++ Cataas.py
@@ -10,6 +10,7 @@ def load_image(url):
         response.raise_for_status()
         image_data = BytesIO(response.content)
         img = Image.open(image_data)
+        img.thumbnail((600,480), Image.Resampling.LANCZOS)
         return ImageTk.PhotoImage(img)
     except Exception as e:
         print(f"Ошибка {e}")
@@ -26,19 +27,15 @@ root = Tk()
 root.title("Cats")
 root.geometry("600x480+400+400")
 
+update_button = Button(text="Обновить", command= set_image)
+update_button.pack()
+
 label = Label()
 label.pack()
 
-update_button = Button(text="Обновить", command= set_image)
-update_button.pack()
 
 url = "https://cataas.com/cat"
 
 set_image()
-# img = load_image(url)
-#
-# if img:
-#     label.config(image=img)
-#     label.image = img
 
 root.mainloop()
