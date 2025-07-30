ab91d75f04266204f5dbc8a6a4f79dc21b6af43e
bmaayandexru <81138044+bmaayandexru@users.noreply.github.com>
2025-07-30 08:14:04 +0300

    отображение по тегу и без него

--- Cataas.py
+++ Cataas.py
@@ -18,12 +18,14 @@ def load_image(url):
 
 
 def open_new_window():
-    img = load_image(url)
+    tag = tag_entry.get()
+    url_tag = f"https://cataas.com/cat/{tag}" if tag else f"https://cataas.com/cat"
+    img = load_image(url_tag)
     if img:
         new_window = Toplevel()
-        new_window.title("Карнка с котиком")
+        new_window.title("Картинка с котиком")
         new_window.geometry("600x480")
-        label = Label(new_window)
+        label = Label(new_window, image=img)
         label.pack()
         label.image = img
 
@@ -33,6 +35,12 @@ def exit():
 root = Tk()
 root.title("Cats")
 root.geometry("600x480+400+400")
+
+tag_entry = Entry(root)
+tag_entry.pack()
+
+load_button = Button(text="Загрузить по тегу", command=open_new_window())
+load_button.pack()
 
 menu_bar = Menu(root)
 root.config(menu = menu_bar)
@@ -48,9 +56,6 @@ file_menu.add_command(label="Выход", command=exit)
 # label = Label()
 # label.pack()
 
-
 url = "https://cataas.com/cat"
-
-set_image()
 
 root.mainloop()
